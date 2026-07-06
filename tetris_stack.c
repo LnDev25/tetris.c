757b527d158cd2393fc54cade328dbaf0b43f14d
Nathan <suporteforlucas@hotmail.com>
2026-07-06 22:38:49 +0000

    feat: implementa pilha de reserva e auto-refill (nivel aventureiro)

--- tetris_stack.c
+++ tetris_stack.c
@@ -1,11 +1,12 @@
-// Nível Novato - Começo //
+// Nível Novato/Aventureiro - Começo //
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-// Capacidade máxima da esteira
-#define MAX 5 
+// Capacidade máxima 
+#define MAX 5 // Capacidade da fila (Esteira)
+#define MAX_PILHA 3 // Capacidade da Pilha (Reserva)
 
 // Struct da Peça
 typedef struct {
@@ -23,7 +24,7 @@ typedef struct {
 
 // Struct da Pilha
 typedef struct {
-    Peca vetor[MAX];
+    Peca vetor[MAX_PILHA];
     int topo;
 } Pilha;
 
@@ -39,49 +40,106 @@ void enqueue(Fila *f, Peca p) ;
 Peca dequeue(Fila *f);
 void mostrarFila(Fila f);
 
+// Motor da pilha (push, pop e mostrarPilha)
+void push(Pilha *p, Peca peca) {
+    if (p->topo == MAX_PILHA -1) {
+        printf("\n [SISTEMA] Reserva cheia") ;
+        return ;
+    } else {
+        p->topo++ ;
+        p->vetor[p->topo] = peca;
+    }
+}
+
+Peca pop(Pilha *p) {
+    if (p->topo == -1) {
+        printf("\n[SISTEMA] Reserva Vazia") ;
+        Peca vazia ;
+        vazia.id = -1;
+        return vazia ;
+    } else {
+        Peca retirada = p->vetor[p->topo];
+        p->topo-- ;
+        return retirada;
+    }
+}
+
+void mostrarPilha(Pilha p) {
+    if (p.topo == -1 ) {
+        printf("\n[Vazia]");
+    } else {
+        for (int i = p.topo; i >= 0; i--) {
+           printf("[%c %d] ", p.vetor[i].nome, p.vetor[i].id); 
+        }
+    }
+}                                                                                                        
+
 // Main
 int main () {
     Fila esteira;
-    inicializarFila(&esteira);
+    inicializarFila(&esteira) ;
+
+    Pilha reserva;
+    inicializarPilha(&reserva) ;
 
     // Aleatoridade
     srand(time(NULL)) ;
+
+    // Pré-preenchendo a esteira com 5 peças (Auto-Refill Inicial)
+    for (int i = 0; i < MAX; i++) {
+        enqueue(&esteira, gerarPeca());
+    }
 
     // Menu
     int opcao = 0;
     do {
 
-        printf ("\n=== TETRIS STACK ===\n");
-        printf ("\n1. Jogar peça (dequeue)") ;
-        printf ("\n2. Inserir nova peça (enqueue)") ;
-        printf ("\n0. Sair.") ;
+        printf("\n1. Jogar peca");
+        printf("\n2. Reservar peca");
+        printf("\n3. Usar peca reservada");
+        printf("\n0. Sair");
         scanf("%d", &opcao);
         
-        switch (opcao)
-        {
-        case 1: {
-            Peca p = dequeue(&esteira); 
-            if (p.id != -1) {
-                printf("\n[JOGO] Voce jogou a peca %c (ID: %d)!\n", p.nome, p.id);
+        switch (opcao) {
+            case 1: { // Jogar Peça
+                Peca p = dequeue(&esteira); 
+                if (p.id != -1) {
+                    printf("\n[JOGO] Voce jogou a peca %c (ID: %d)!\n", p.nome, p.id);
+                    enqueue(&esteira, gerarPeca());
                 }
+                break;
             }
-            break;
 
-        case 2: {
-            Peca nova = gerarPeca();
-            enqueue(&esteira, nova);
+            case 2: { // Reservar Peça
+                if (reserva.topo < MAX_PILHA - 1) {
+                    Peca p = dequeue(&esteira);
+                    push(&reserva, p);
+                    enqueue(&esteira, gerarPeca());
+                } else {
+                    printf("\n[SISTEMA] Operacao cancelada: A reserva esta cheia!\n");
+                }
+                break;
             }
-            break;
 
-        case 0:
-            printf("\nSaindo do Tetris Stack...\n");
-            break;
+            case 3: { // Usar Reserva
+                Peca p = pop(&reserva);
+                if (p.id != -1) {
+                    printf("\n[JOGO] Voce usou a reserva: %c (ID: %d)!\n", p.nome, p.id);
+                }
+                break;
+            }
 
-        default:
-            break;
+            case 0:
+                printf("\nSaindo do Tetris Stack...\n");
+                break;
+
+            default:
+                printf("\nOpcao Invalida!\n");
+                break;
         }
 
     mostrarFila(esteira) ;
+    mostrarPilha(reserva) ;
 
     } while (opcao != 0);
     printf("\n");
@@ -92,9 +150,7 @@ int main () {
 // Motor de partida 
 void inicializarFila(Fila *f) {
     f->inicio = 0;
-
     f->fim = -1;
-
     f->total = 0;
 }
 
@@ -123,7 +179,6 @@ void enqueue(Fila *f, Peca p) {
         f->fim = (f->fim + 1) % MAX;
         f->vetor[f->fim] = p;
         f->total++;
-        printf("[SISTEMA] Sucesso") ;
     }
 }
 
@@ -154,4 +209,4 @@ void mostrarFila(Fila f) {
     }
 }
 
-// Nível Novato - Fim //
+// Nível Novato/Aventureiro - Fim //
